75004c98e2f45e09ca06f1e71b4b8dc0c34a4570
veysel basbaydar <vgbasbaydar@gmail.com>
2022-03-12 19:46:30 -0800

    added more dummy data

--- assets/images/images.js
+++ assets/images/images.js
@@ -2,4 +2,10 @@ export default {
   HOME_1: require("../images/home/home_1.jpeg"),
   HOME_2: require("../images/home/home_2.jpeg"),
   HOME_3: require("../images/home/home_3.jpeg"),
-}
+  HOME_4: require("../images/home/home_4.jpeg"),
+  HOME_5: require("../images/home/home_5.jpeg"),
+  HOME_6: require("../images/home/home_6.jpeg"),
+  HOME_7: require("../images/home/home_7.jpeg"),
+  HOME_8: require("../images/home/home_8.jpeg"),
+  HOME_9: require("../images/home/home_9.jpeg"),
+};
--- data.js
+++ data.js
@@ -15,21 +15,35 @@ const demoHomeSliders_1 = [
   },
   {
     id: "home_slider_4",
-    image: Images.HOME_3,
+    image: Images.HOME_4,
   },
 ];
 
 const demoHomeSliders_2 = [
   {
-    id: "home_slider_4",
-    image: Images.HOME_1,
-  },
-  {
     id: "home_slider_5",
-    image: Images.HOME_2,
+    image: Images.HOME_5,
   },
   {
     id: "home_slider_6",
+    image: Images.HOME_6,
+  },
+  {
+    id: "home_slider_7",
+    image: Images.HOME_7,
+  },
+];
+const demoHomeSliders_3 = [
+  {
+    id: "home_slider_8",
+    image: Images.HOME_8,
+  },
+  {
+    id: "home_slider_9",
+    image: Images.HOME_9,
+  },
+  {
+    id: "home_slider_3",
     image: Images.HOME_3,
   },
 ];
@@ -39,25 +53,31 @@ const demoData = [
     id: "flat_list_home_1",
     images: demoHomeSliders_1,
     name: "Maslak 42 Tower",
-    address: "Maslak Mahallesi, Levent, İstanbul",
+    address: "Maslak Street, Levent, İstanbul",
   },
   {
     id: "flat_list_home_2",
     images: demoHomeSliders_2,
     name: "Maslak 43 Tower",
-    address: "Maslak 43 Mahallesi, Levent, İstanbul",
+    address: "Maslak 43 Street, Uskudar, İstanbul",
   },
   {
     id: "flat_list_home_3",
     images: demoHomeSliders_1,
     name: "Maslak 44 Tower",
-    address: "Maslak Mahallesi, Levent, İstanbul",
+    address: "Maslak Street, Kadikoy, İstanbul",
   },
   {
     id: "flat_list_home_4",
     images: demoHomeSliders_2,
     name: "Maslak 45 Tower",
-    address: "Maslak 45 Mahallesi, Levent, İstanbul",
+    address: "Maslak 45 Street, Umraniye, İstanbul",
+  },
+  {
+    id: "flat_list_home_5",
+    images: demoHomeSliders_3,
+    name: "Maslak 45 Tower",
+    address: "Maslak 45 Street, Atasehir, İstanbul",
   },
 ];
 
